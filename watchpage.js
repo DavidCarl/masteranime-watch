39165caea7ccd9932ac73c991bea4011675647e9
unknown <david.martin.carl@gmail.com>
2019-02-23 16:47:44 +0100

    Support for removing watched episode

--- watchpage.js
+++ watchpage.js
@@ -1,17 +1,9 @@
 myStorage = window.localStorage;
 
-console.log('ADDON: Loaded!')
-
-let url = window.location.href;
-
-if (url.match('masterani.me\/anime\/watch.+')) {
+function main(){
     removeAd();
-    console.log('ADDON: Anime Watch page!');
-} else if(url.match('masterani.me\/anime\/info.+')) {
-    clear_data()
-    console.log('ADDON: Anime Info page!');
-}else{
-    console.log('ADDON: Fail');
+    testasdasdasdasd();
+    console.log('ADDON: Anime page!');
 }
 
 function removeAd() {
@@ -23,18 +15,12 @@ function removeAd() {
             var button = document.createElement('button');
             button.id = 'addaswatched';
             bText = 'Add as watched';
-            try {
-                if (myStorage.getItem(getAnimeName()).includes(getAnimeEpisode())) {
-                    bText = 'Remove as watched';
-                }
-            } catch (error) {
-                console.log('ADDON: ' + error)
+            if (myStorage.getItem(getAnimeName()).includes(getAnimeEpisode())) {
+                bText = 'Remove as watched';
             }
-
             button.appendChild(document.createTextNode(bText));
             place.appendChild(button);
             place.appendChild(watch_status());
-            testasdasdasdasd();
         }
     }
 }
@@ -42,10 +28,12 @@ function removeAd() {
 function getAnimeEpisode() {
     let info = document.getElementsByClassName('info')[1];
     return info.children[1].textContent.match('\\d+')[0];
+    //return anime_name;
 }
 
 function getAnimeName() {
     let info = document.getElementsByClassName('info')[1];
+    //let anime_name = 
     return info.children[0].textContent;
 }
 
@@ -54,13 +42,9 @@ function testasdasdasdasd() {
         let info = document.getElementsByClassName('info')[1];
         let anime_name = info.children[0].textContent;
         let anime_episode = info.children[1].textContent.match('\\d+')[0];
-        try {
-            if (myStorage.getItem(anime_name).includes(anime_episode)) {
-                remove_anime_episode(anime_name, anime_episode);
-            } else {
-                add_anime_episode(anime_name, anime_episode);
-            }
-        } catch (error) {
+        if (myStorage.getItem(getAnimeName()).includes(getAnimeEpisode())) {
+            remove_anime_episode(anime_name, anime_episode);
+        } else {
             add_anime_episode(anime_name, anime_episode);
         }
     });
@@ -91,13 +75,9 @@ function add_anime_episode(anime_name, episode) {
     let arr_old = myStorage.getItem(anime_name);
     let arr_new = [];
     if (arr_old != undefined) {
-        if (!arr_old.includes(episode)) {
-            arr_new.push(episode);
-        } else {
-            console.log('ADDON: Episode already added!')
-        }
+        arr_new.push(arr_old);
     }
-    arr_new.push(arr_old);
+    arr_new.push(episode);
     myStorage.setItem(anime_name, arr_new);
     removeAd();
 }
@@ -105,35 +85,20 @@ function add_anime_episode(anime_name, episode) {
 function remove_anime_episode(anime_name, episode) {
     let arr_old = myStorage.getItem(anime_name);
     if (arr_old != undefined && myStorage.getItem(getAnimeName()).includes(getAnimeEpisode())) {
-        arr_old = remove_array_element(arr_old, episode);
+        arr_old = arr_remove(arr_old, episode);
     }
     myStorage.setItem(anime_name, arr_old);
     removeAd();
 }
 
-function remove_array_element(arr, n) {
-    try {
-        var sarr = arr.split(',')
-        var index = sarr.indexOf(n);
-        if (index > -1) {
-            sarr.splice(index, 1);
-        }
-    } catch (error) {
-        console.log('ADDON: ' + error)
+function arr_remove(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
     }
-    return sarr;
+    return arr
 }
-
 
 function clear_data() {
-    anime_titel = document.getElementsByClassName('right floated sixteen wide mobile ten wide tablet twelve wide computer column')[0].children[0].childNodes[0].data
-    let regex = /[ \t]+$/
-    console.log(myStorage.getItem(anime_titel.replace(regex, '')))
-    console.log(anime_titel.replace(regex, ''))
-}
 
-
-
-function main_info(){
-    title = document.getElementsByClassName('title')
 }
